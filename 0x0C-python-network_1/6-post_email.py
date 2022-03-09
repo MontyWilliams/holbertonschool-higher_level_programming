6b45a80c4b13307776496ca13e83e3f5273439d1
MontyWilliams <3650@holbertonschool.com>
2022-03-08 23:45:33 -0600

    post an email using py requst and check for error

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL and an
+    email address, sends a POST request to the passed
+    URL with the email as a parameter, and finally displays
+    the body of the response.
+"""
+
+if __name__ == '__main__':
+
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    r = requests.post(url, data={'email': email})
+    if r.status_code == 400:
+        print(f"Error code: {r.status_code}")
