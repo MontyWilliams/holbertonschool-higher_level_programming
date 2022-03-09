47157591798405500c20b32aea8d997c64a1692d
MontyWilliams <3650@holbertonschool.com>
2022-03-08 22:59:19 -0600

    moved import statement

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import urllib.request
 """fetch url status
 """
 if __name__ == "__main__":
-    import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
