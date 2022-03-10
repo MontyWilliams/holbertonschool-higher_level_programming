eaea75b3e86c2186ad9c059f93f20695ab51bb86
MontyWilliams <3650@holbertonschool.com>
2022-03-10 11:24:27 -0600

    changing git permission using git add --chmod=+x -- *

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -4,11 +4,11 @@
 
 
 if __name__ == "__main__":
-    import urllib.request
+    from urllib import request
     import sys
 
     url = sys.argv[1]
     email = sys.argv[2]
     payload = {'email': email}
-    with urllib.request.urlopen(url, data=bytes(str(payload), 'utf-8')) as response:
+    with request.urlopen(url, data=bytes(str(payload), 'utf-8')) as response:
         print(str(response.read(), 'utf-8'))
