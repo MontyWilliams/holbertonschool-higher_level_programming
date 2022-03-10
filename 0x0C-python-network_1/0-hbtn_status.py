4e4adcd5b6d189222f10f079f99565a03d6260bb
MontyWilliams <3650@holbertonschool.com>
2022-03-10 11:23:10 -0600

    fixed syntax

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@
 if __name__ == "__main__":
     from  urllib import request
 
-    with request.urlopen('https://api.github.com/events') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    # print("Body response:")
-    # print("\t- type: {}".format(type(html)))
-    # print("\t- content: {}".format(html))
-    # print("\t- utf8 content: {}".format(html.decode('UTF8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('UTF8')))
