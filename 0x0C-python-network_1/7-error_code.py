cddded629e6945f925724e03a0eecbd95687d067
Montywilliams <3650@holbertonschool.com>
2022-03-10 23:30:39 -0600

    changed yntax, calling argv using get

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -6,10 +6,11 @@
 """
 
 if __name__ == '__main__':
-    import requests
+    from requests import get
     from sys import argv
 
-    url = argv[1]
-    r = requests.get(url)
-    if r.status_code == 400:
-        print(f"Error code: {r.status_code}")
+    request = get(argv[1])
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
+    else:
+        print(request.text)
