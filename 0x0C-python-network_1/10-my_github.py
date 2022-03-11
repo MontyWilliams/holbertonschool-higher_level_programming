f1875feeb5ca41817122f7ddec453e2758ae51c3
Montywilliams <3650@holbertonschool.com>
2022-03-10 23:56:31 -0600

    readded sysimport

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -5,12 +5,12 @@ script that fetches https://api.github.com/user
 
 
 if __name__ == "__main__":
-    from sys import argv
-    from requests.auth import HTTPBasicAuth
     import requests
+    import sys
+    from requests.auth import HTTPBasicAuth
 
-    token = argv[2]
-    user = argv[1]
+    token = sys.argv[2]
+    user = sys.argv[1]
     url = "https://api.github.com/user"
 
     r = requests.get(url, auth=HTTPBasicAuth(user, token)).json()
