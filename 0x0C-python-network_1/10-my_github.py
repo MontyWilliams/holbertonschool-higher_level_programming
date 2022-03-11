14a8129f5988cfb8238f81d7cca363cde4078be2
Montywilliams <3650@holbertonschool.com>
2022-03-10 23:53:19 -0600

    readded HTTPBAsicAuth and stored ars as variables to be passed to it

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     from requests.auth import HTTPBasicAuth
     import requests
 
-    token = sys.argv[2]
-    user = sys.argv[1]
+    token = argv[2]
+    user = argv[1]
     url = "https://api.github.com/user"
 
     r = requests.get(url, auth=HTTPBasicAuth(user, token)).json()
