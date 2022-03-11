8077f3683f4d35ea91a3d125559719e2991a15df
Montywilliams <3650@holbertonschool.com>
2022-03-10 23:48:21 -0600

    changed syntax deleted httpauth

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -6,10 +6,9 @@ script that fetches https://api.github.com/user
 
 if __name__ == "__main__":
     from requests import get
-    from requests.auth import HTTPBasicAuth
     from sys import argv
 
     requests = get('https://api.github.com/user',
-                   auth=HTTPBasicAuth(argv[1], argv[2])).json()
-    print(requests.get('id'))
+                   auth=(argv[1], argv[2]))
+    print(requests.json['id'])
 
