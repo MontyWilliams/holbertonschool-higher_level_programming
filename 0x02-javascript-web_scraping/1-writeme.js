6fed877e08e5594d662be6955d0b7597f4614bee
Montywilliams <3650@holbertonschool.com>
2022-04-07 14:44:32 -0600

    removed logic that prints to the console

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,5 @@ const text = process.argv[3];
 _request.writeFile(file, text, 'utf-8', function (error, content) {
   if (error) {
     console.error(error);
-  } else {
-    console.log(content);
   }
 });
