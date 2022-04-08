2ca7e79dd80b3088ad02fea35b4a3b5c14ffa5c4
Montywilliams <3650@holbertonschool.com>
2022-04-07 21:28:05 -0600

    removed unused variable id

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
 const _url = 'https://swapi-api.hbtn.io/api/films/';
 
 request(_url, (error, response, body) => {
